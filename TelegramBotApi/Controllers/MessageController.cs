5d19b3ffe2ecd7804d61a3473cd61400cc2d5157
Taha Haşemi <71307248+Taha-Hasemi@users.noreply.github.com>
2022-01-27 20:46:00 +0330

    first commit

---
+++ TelegramBotApi/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBotApi.Controllers
+{
+    [Route("api/message")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            TelegramBotClient client = new TelegramBotClient("5182535092:AAGBgIFmb-oQ5HBXiv-Nx8tQ78jOejFV68o");
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "Mesaj alındı");
+            }
+            return Ok();
+        }
+    }
+}
