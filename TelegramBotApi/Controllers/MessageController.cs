f68944162b103c85dc7994ffe152eec18697e4a8
Taha Haşemi <71307248+Taha-Hasemi@users.noreply.github.com>
2022-02-02 16:56:56 +0330

    Configure project by Dependency Injection

--- TelegramBotApi/Controllers/MessageController.cs
+++ TelegramBotApi/Controllers/MessageController.cs
@@ -11,10 +11,17 @@ namespace TelegramBotApi.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
+        Bot _bot;
+
+        public MessageController(Bot bot)
+        {
+            _bot = bot;
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Update update)
         {
-            TelegramBotClient client = Bot.GetClient();
+            TelegramBotClient client = _bot.GetClient();
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 await client.SendTextMessageAsync(update.Message.From.Id, "Mesaj alındı");
--- TelegramBotApi/Models/Bot.cs
+++ TelegramBotApi/Models/Bot.cs
@@ -12,12 +12,13 @@ namespace TelegramBotApi.Models
         public Bot(IConfiguration configuration)
         {
             _configuration = configuration;
+            _botsettings = _configuration.GetSection("BotSettings").Get(typeof(BotSettings)) as BotSettings;
         }
 
-        private static TelegramBotClient _client;
-        private static BotSettings _botsettings;
+        private TelegramBotClient _client;
+        private BotSettings _botsettings;
 
-        public static TelegramBotClient GetClient()
+        public TelegramBotClient GetClient()
         {
             if (_client != null)
             {
@@ -29,7 +30,6 @@ namespace TelegramBotApi.Models
 
         public async Task SetWebhook()
         {
-            _botsettings = _configuration.GetSection("BotSettings").Get(typeof(BotSettings)) as BotSettings;
             string webHookUrl = string.Format(_botsettings.BotUrl + "/{0}", "api/message");
             _client = new TelegramBotClient(_botsettings.BotToken);
             await _client.SetWebhookAsync(webHookUrl);
